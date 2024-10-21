9bae421b5d726295bd692dee5971a49b8f583cfa
MukeshR-prog <mukeshr1855@gmail.com>
2024-10-21 09:09:46 +0530

    seventh change

--- src/component/AmmenitiesUtils.jsx
+++ src/component/AmmenitiesUtils.jsx
@@ -5,7 +5,7 @@ import book from '../assets/book.svg'
 import { LiaSwimmingPoolSolid } from "react-icons/lia";
 function AmmenitiesUtils({ammenities}) {
   return (
-    <Box sx={{alignItems:'center',display:'flex',width:'370px',height:'32px',justifyContent:'space-between',padding:'15px',backgroundColor:'red',borderRadius:'5px',backgroundColor:ammenities.color}}>
+    <Box sx={{alignItems:'center',display:'flex',width:'370px',height:'32px',justifyContent:'space-between',padding:'15px',borderRadius:'5px',backgroundColor:ammenities.color}}>
         <Box sx={{display:'flex',gap:'10px',alignItems:'center'}}>
             <Box sx={{fontSize:'35PX',color:'#B3776D',marginTop:'15px'}}>{ammenities.icon}</Box>
             <Typography sx={{fontSize:'16px', color:ammenities.bold}}>{ammenities.title}</Typography>
--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Popupside1 from "./component/Popupside1.jsx";
-import Popupside1delete from "./component/Popupside1delete.jsx";
-import PriceList from "./component/PriceList.jsx";
-import Unitpricedetails from "./component/Unitpricedetails.jsx";
-import Addamenities from "./component/Addamenities.jsx";
-import AmmenitiesUtils from "./component/AmmenitiesUtils.jsx";
-import Displayammenities from "./component/Displayammenities.jsx";
-import Primesecond from "./component/Primesecond.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-   <Displayammenities/>
-    {/* <Addamenities/> */}
-    <Primesecond/>
-    <Unitpricedetails />
-    {/* <PriceList />
-   
-    <Popupside1delete />
-    <Popupside1 /> */}
   </StrictMode>
 );
